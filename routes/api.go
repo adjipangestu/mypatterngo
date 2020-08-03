f57c41bd48f8a31025407403907a4eaf9112e599
Adji Pangestu <adji.pangestu@detik.com>
2020-08-04 06:07:02 +0700

    delete route

--- routes/api.go
+++ routes/api.go
@@ -35,20 +35,4 @@ func (api *Api) InitializeRoutes() {
 
 	// Open Routes
 	GuestRouter.HandleFunc("/welcome", controllers.Welcome).Methods("GET")
-	//PublicRouter.HandleFunc("/login", controllers.Login).Methods("POST")
-	//PublicRouter.HandleFunc("/forgot-password", controllers.ForgotPassword).Methods("POST")
-	//PublicRouter.HandleFunc("/change-password/{token}", controllers.ChangePassword).Methods("PATCH")
-
-	// High Admin Routes
-	//ProtectedRouterHighAdminRouter.HandleFunc("/roles", controllers.GetAllRoles).Methods("GET")
-	//ProtectedRouterHighAdminRouter.HandleFunc("/roles", controllers.CreateRole).Methods("POST")
-	//ProtectedRouterHighAdminRouter.HandleFunc("/roles/{id}", controllers.GetRole).Methods("GET")
-	//ProtectedRouterHighAdminRouter.HandleFunc("/roles/{id}", controllers.UpdateRole).Methods("PATCH")
-	//ProtectedRouterHighAdminRouter.HandleFunc("/roles/{id}", controllers.DeleteRole).Methods("DELETE")
-	//ProtectedRouterHighAdminRouter.HandleFunc("/users", controllers.GetAllUsers).Methods("GET")
-
-	// Protected Routes
-	//ProtectedRouter.HandleFunc("/users/me", controllers.GetAuthenticatedUser).Methods("GET")
-	//ProtectedRouter.HandleFunc("/users/me/upload-image", controllers.UploadUserImage).Methods("PATCH")
-	//ProtectedRouter.HandleFunc("/users/me/delete-image", controllers.DeleteImage).Methods("DELETE")
 }
